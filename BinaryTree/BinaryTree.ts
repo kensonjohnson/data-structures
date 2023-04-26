0d200b772cce384ba78e2da1112960a249fa0c36
Kenson Johnson <kenson.johnson@outlook.com>
2023-04-26 15:30:53 -0500

    Started implementation of deleteOne

--- BinaryTree/BinaryTree.ts
+++ BinaryTree/BinaryTree.ts
@@ -1,3 +1,9 @@
+type QueueNode = {
+  parent: TreeNode;
+  node: TreeNode;
+  direction: "left" | "right" | "root";
+};
+
 class TreeNode {
   value: number | string;
   right: null | TreeNode;
@@ -59,9 +65,66 @@ class BinaryTree {
   }
 
   // Delete
-  // Use postorder traversal and delete the first node of given value
   // This favors deleting the "lowest" node in the tree
-  deleteOne(value: number | string) {}
+  deleteOne(value: number | string) {
+    if (value === null || typeof value === "undefined") {
+      return false;
+    }
+
+    // level order search to find the first node with value given
+    const queue: QueueNode[] = [];
+    queue.push({
+      parent: this.root as TreeNode,
+      node: this.root as TreeNode,
+      direction: "root",
+    });
+    while (queue.length) {
+      const currentNode = queue.shift() as QueueNode;
+      const { parent, node } = currentNode;
+
+      if (node.value === value) {
+        // Handle delete of leaf node
+        if (!node.left && !node.right) {
+          if (currentNode.direction === "left") {
+            parent.left = null;
+            return true;
+          }
+          parent.right = null;
+          return true;
+        }
+
+        // Handle delete of non-leaf node
+
+        // Searching for the rightmost and lowest node in subtree
+        let replacementNode = node;
+        while (true) {
+          if (replacementNode.right) {
+            replacementNode = replacementNode.right;
+            continue;
+          }
+          if (replacementNode.left) {
+            replacementNode = replacementNode.left;
+            continue;
+          }
+          break;
+        }
+
+        // This is where we stopped on 4/26/23
+      } else {
+        if (node.left) {
+          queue.push({ parent: node, node: node.left, direction: "left" });
+        }
+
+        if (node.right) {
+          queue.push({
+            parent: node,
+            node: node.right,
+            direction: "right",
+          });
+        }
+      }
+    }
+  }
 
   deleteAll(value: number | string) {}
 
